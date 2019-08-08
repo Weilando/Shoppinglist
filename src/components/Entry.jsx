ec608e9f17387934885afff360d4d7582c940cc1
Weilando <Hendryk.Weiland@hhu.de>
2019-08-09 00:31:10 +0200

    feat/wip: lift entrie's state up and add editing (wip)

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -2,18 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Entry extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   static propTypes = {
     eContent: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
   };
 
-  static defaultProps = {
-    eContent: 'Cheese...',
-  };
+  handleChange(event) {
+    this.props.onChange(this.key, event.target.value);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.key, event.target.value);
+  }
 
   render() {
     return (
       <li>
-        {this.props.eContent}
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.props.eContent}
+            onChange={this.handleChange}
+            onBlur={this.handleSubmit}
+          />
+          <input type='reset' value='Remove' name='reset' />
+        </form>
       </li>
     );
   }
--- src/components/Shoppinglist.jsx
+++ src/components/Shoppinglist.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Entry from './Entry';
 
 class Shoppinglist extends React.Component {
@@ -6,9 +7,57 @@ class Shoppinglist extends React.Component {
     super(props);
 
     this.state = {
-      contents: [ 42, 'Cheese', 3, 'Bananas', 'Potatoes', 'Coke', 'Sparling Water' ],
-      //entries: Array(5).fill(null),
+      contents: this.props.contents,
     };
+
+    this.updateEntry = this.updateEntry.bind(this);
+    this.deleteEntry = this.deleteEntry.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  static propTypes = {
+    contents: PropTypes.array,
+  }
+
+  static defaultProps = {
+    contents: [ 42, 'Cheese', 3, 'Bananas', 'Potatoes', 'Coke', 'Sparling Water' ],
+  }
+
+  deleteEntry(key) {
+    let { oldContents } = this.state;
+    const newContents = Array(oldContents.length-1);
+
+    if(key >= oldContents.length) return null;
+
+    for(let i=0; i<key; i++) {
+      newContents[i] = oldContents[i];
+    }
+    for(let i=key; i<oldContents.length-1; i++) {
+      newContents[i] = oldContents[i+1];
+    }
+
+    this.setState({contents: newContents});
+  }
+
+  updateEntry(key, content) {
+    const newContents = this.state.contents.slice();
+
+    newContents[key] = content;
+
+    this.setState({contents: newContents});
+  }
+
+  onChange(key, value) {
+    this.updateEntry(key, value);
+  }
+
+  onSubmit(key, value) {
+    if(value === '') {
+      this.deleteEntry(key);
+    } else {
+      this.updateEntry(key, value);
+    }
   }
 
   render() {
@@ -18,8 +67,13 @@ class Shoppinglist extends React.Component {
       keys[i] = i;
     }
 
-    const listItems = keys.map((tmpKey) =>
-      <Entry key={tmpKey} eContent={String(contents[tmpKey])} />
+    let listItems = keys.map((tmpKey) =>
+      <Entry
+        key={tmpKey}
+        eContent={String(contents[tmpKey])}
+        onChange={this.onChange}
+        onSubmit={this.onSubmit}
+      />
     );
 
     return (
