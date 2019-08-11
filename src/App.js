fd5f757c97f539b8fbe221f101925e14d11e71fc
Weilando <Hendryk.Weiland@hhu.de>
2019-08-11 22:23:32 +0200

    refactor: add style

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Shoppinglist from './components/Shoppinglist'
 function App() {
   return (
     <div className="App">
-      <h1>Einkaufsliste</h1>
+      <h1>Shoppinglist</h1>
       <Shoppinglist />
     </div>
   );
